e6df1d76af6d1ff1435b5e13104c26713f235bac
KevinLindemark <58036568+KevinLindemark@users.noreply.github.com>
2023-03-29 17:01:14 +0200

    Add files via upload

--- UART_ESP32_RPI/ESP32/main.py
+++ UART_ESP32_RPI/ESP32/main.py
@@ -1,5 +1,6 @@
 from machine import UART
 from time import sleep
+import sys
 from random import randint
 uart = UART(1, 115200) # 1st argument: UART number: Hardware UART #1
 
@@ -10,5 +11,9 @@ while True:
     randiddy = randint(0, 99)
     print(f"hello random number is: {str(randiddy)}")
     # Write
-    uart.write(f"hello random number is: {str(randiddy)}")
-    sleep(1)
+    try:
+        uart.write(f"hello random number is: {str(randiddy)} \n")
+        sleep(1)
+    except KeyboardInterrupt:
+        print("Exit!")
+        sys.exit()
